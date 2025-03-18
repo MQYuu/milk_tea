c02b8c476023ab56f91a937a3874269075d19460
MinhQuang-dev <phanquang704@gmail.com>
2025-03-18 21:50:41 +0700

    git commit login done

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="login-container">
-    <div class="login-box">
+    <div v-if="isLoggedIn" class="profile-box">
+      <h1>Chào mừng, {{ userInfo.email }}</h1>
+      <button class="logout-button" @click="handleLogout">Đăng xuất</button>
+    </div>
+    <div v-else class="login-box">
       <h1>Đăng nhập</h1>
       <form @submit.prevent="handleLogin">
         <div class="form-group">
@@ -22,25 +26,45 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { loginUser } from '../api/users';
 
 const user = ref({ email: '', password: '' });
+const userInfo = ref({});
+const isLoggedIn = ref(false);
 const router = useRouter();
+
+// Kiểm tra nếu đã đăng nhập thì hiển thị thông tin
+onMounted(() => {
+  const storedUser = localStorage.getItem("userInfo");
+  if (storedUser) {
+    userInfo.value = JSON.parse(storedUser);
+    isLoggedIn.value = true;
+  }
+});
 
 const handleLogin = async () => {
   try {
     const response = await loginUser(user.value.email, user.value.password);
     if (response.token) {
       localStorage.setItem("userToken", response.token);
-      router.push("/profile");
+      localStorage.setItem("userInfo", JSON.stringify({ email: user.value.email }));
+      userInfo.value = { email: user.value.email };
+      isLoggedIn.value = true;
     } else {
       alert("Đăng nhập thất bại");
     }
   } catch (error) {
     console.error("Lỗi đăng nhập:", error);
   }
+};
+
+const handleLogout = () => {
+  localStorage.removeItem("userToken");
+  localStorage.removeItem("userInfo");
+  isLoggedIn.value = false;
+  userInfo.value = {};
 };
 </script>
 
